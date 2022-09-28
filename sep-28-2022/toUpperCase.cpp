ac00d09177e8b842e97760e68a6f4fd63515a631
ArmenParanyan <80101033+ArmParanyan@users.noreply.github.com>
2022-09-28 15:42:13 +0400

    Add files via upload

---
+++ sep-28-2022/toUpperCase.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+
+std::string toUpperCase(std::string&);
+
+int main()
+{
+    std::string str = "HeLlO";
+    std::string res = toUpperCase(str);
+    std::cout << res << std::endl;
+    return 0;
+}
+
+std::string toUpperCase(std::string& str)
+{
+    for (int i = 0; i < str.size(); ++i)
+    {
+        if (str[i] >= 'a' && str[i] <= 'z')
+        {
+            str[i] -=32;
+        }
+    }
+    return str;
+}
